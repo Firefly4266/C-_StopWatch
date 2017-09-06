c1c27925345c06c613a6d35653b40ba91c897713
Firefly4266 <gaines.johnp@gmail.com>
2017-09-05 19:56:54 -0700

    Stopwatch exercise completed.

---
+++ StopWatch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter to start timer.");
+            Console.ReadLine();
+
+            StopWatch watch = new StopWatch();
+            watch.Start();
+
+            Console.WriteLine("Press enter to stop the timer.");
+            Console.ReadLine();
+            watch.Stop();
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class StopWatch
+    {
+        private DateTime startTime;
+        public void Start()
+        {
+            startTime = DateTime.Now; 
+        }
+        public void Stop()
+        {
+            TimeSpan elapsed = DateTime.Now - startTime;
+            Console.WriteLine("The elapsed time is: {0}s {1}ms.", elapsed.Seconds, elapsed.Milliseconds);
+        }
+    }
+}
